f5f0bafa88b31df224fc27f806cf5edae5257a6e
Jaemin Kim <jaemin@esl.mju.ac.kr>
2020-10-14 19:03:44 +0900

    Update main.c
    괜히 한번 해 봤음.

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
+    //Git hub 는 바로 수정도 됩니다!
+    //완소 git hub.
+    printf ("Wow!\r\n");
+    
     printf ("ABCDEFG!\r\n");
     printf ("HIJKLMNOP!\r\n");
     return 1;
