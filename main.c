05289252ea2ffe878089303484a61a29f914a715
Jaemin Kim <jaemin@esl.mju.ac.kr>
2020-10-14 19:12:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,10 @@ int main(void)
 {
     //Git hub 는 바로 수정도 됩니다!
     //완소 git hub.
-    printf ("Wow!\r\n");
+    
+    
+    //Modified.
+    printf ("Wowwwww!!!!\r\n");
     
     printf ("ABCDEFG!\r\n");
     printf ("HIJKLMNOP!\r\n");
